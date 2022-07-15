519d379c0034ba909c6bd0e1536998a4451b6a24
Hanuman Ghawade <hanuman.ghawade@excellarate.com>
2022-07-15 13:52:43 +0530

    date file added

---
+++ dates15-7.js
@@ -0,0 +1,21 @@
+// JavaScript Date Objects
+
+const d = new Date()
+console.log(d)
+
+const d1 = new Date(2015,10,25,24,15,30,33)
+console.log(d1)
+
+// to convert string 
+
+console.log(d.toString())
+
+// to UTC string
+
+console.log(d.toUTCString())
+
+// to date string
+
+console.log(d.toDateString())
+
+
