9129dffae24e6abf7acb9abd45bc92275b467f1e
Hanuman Ghawade <hanuman.ghawade@excellarate.com>
2022-07-18 14:30:48 +0530

    break_continue file added

---
+++ break_continue18-7.js
@@ -0,0 +1,23 @@
+
+// break statements in JS
+let number = ''
+for (let i = 0; i < 10; i++) {
+    if (i==5) {
+        break
+    }
+    number +=  ' ' + i ;
+}
+console.log(number)
+
+
+// Continue statements in JS
+
+let number1 = ''
+for (let i = 0; i < 10; i++) {
+    if (i==5) {
+       continue
+    }
+    number1 +=  ' ' + i ;
+}
+console.log(number1)
+
