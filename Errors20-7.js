4ace04a72c5153d36e8ef628d3408be14a8f6856
Hanuman Ghawade <hanuman.ghawade@excellarate.com>
2022-07-20 14:48:21 +0530

    Fils Added

--- Errors20-7.js
+++ Errors20-7.js
@@ -7,6 +7,23 @@ Erros in Javscript :  Throw, and Try...Catch...Finally
 4.throw statement defines a custom error.
 */
 
-"use strict"
-y = 25 ;
-console.log(y)
+function division (a ,b){
+    return a / b 
+
+}
+try {
+   let div = division(5, 5) ;
+   console.log(div) ;
+} catch (Erros) {
+
+    console.log(Erros.message) ;
+}
+
+finally {
+
+    console.log(" Please always keep in mind value should not be 0 ") ;
+}
+
+
+
+
