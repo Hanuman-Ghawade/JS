bf6475a65df888ebd2515e795ebe705d3b9813b5
Hanuman Ghawade <hanuman.ghawade@excellarate.com>
2022-07-21 19:31:47 +0530

    Modules,map,filter file added

--- classes20-7.js
+++ classes20-7.js
@@ -1,37 +1,38 @@
 // JavaScript Classes
 // Employee class created 
 
+
 class Employee {
-    constructor(name,age,place){
-        this.name = name ;
-        this.age = age ;
-        this.place = place ;
+    constructor(name, age, place) {
+        this.name = name;
+        this.age = age;
+        this.place = place;
     }
 
-    getDetails(){
+    getDetails() {
         console.log(`My name is ${this.name} .My age is ${this.age} year . I am from ${this.place}`,)
     }
 
 }
 
-class HumanResource extends Employee{
-    constructor(name, education ){
-        
-        super(name) ;
-        this.education = education ;
+class HumanResource extends Employee {
+    constructor(name, education) {
+
+        super(name);
+        this.education = education;
     }
 
-    qualification(){
+    qualification() {
         console.log(`My name ${this.name} & i have studied ${this.education}`)
 
     }
 
 
 }
-let parent_obj = new Employee("Hanuman",28, "Beed")
+let parent_obj = new Employee("Hanuman", 28, "Beed")
 parent_obj.getDetails()
 
-let child_obj = new HumanResource("Hanuman","Mechanical Enginnering")
+let child_obj = new HumanResource("Hanuman", "Mechanical Enginnering")
 
 child_obj.qualification()
 
@@ -40,16 +41,16 @@ child_obj.qualification()
 //  getter & setter method 
 
 class person {
-    constructor(name){
-        this._name = name ;
+    constructor(name) {
+        this._name = name;
     }
-    set person_name(x){
+    set person_name(x) {
         this._name = x
     }
     get person_name() {
         console.log(`My name is ${this._name}`)
     }
-  
+
 }
 
 person_first = new person("Poonam")
@@ -57,19 +58,19 @@ person_first.person_name = "Deepak";
 person_first.person_name
 
 
- // static method 
+// static method 
 
- class Dog{
-    constructor(breed){
+class Dog {
+    constructor(breed) {
         this.breed = breed
     }
-    static behaviour(){
+    static behaviour() {
         console.log("The German Shepherd or Alsatian is a German breed of \
 working dog of medium to large size.")
 
     }
 
- }
+}
 
 dog_obj = new Dog("German Shepherd")
 Dog.behaviour()
